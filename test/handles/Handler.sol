c43baad4e0017a429dc543491e4114e96eb78f3a
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-03-13 19:44:35 +0530

    invariant runs

--- test/handles/Handler.sol
+++ test/handles/Handler.sol
@@ -6,9 +6,10 @@ import {CommonBase} from "forge-std/Base.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
 import {StdUtils} from "forge-std/StdUtils.sol";
 
+import "forge-std/console2.sol";
+
 contract Handler is CommonBase, StdCheats, StdUtils {
     WETH9 public weth;
-
     uint256 public constant ETH_SUPPLY = 120_500_000 ether;
 
     constructor(WETH9 _weth) {
@@ -17,12 +18,15 @@ contract Handler is CommonBase, StdCheats, StdUtils {
     }
 
     function deposit(uint256 amount) public {
+        // amount = bound(amount, 0, 10);
         amount = bound(amount, 0, address(this).balance);
         weth.deposit{value: amount}();
     }
 
     function withdraw(uint256 amount) public {
-        bound(amount, 0, weth.balanceOf(address(this)));
+        // bound(amount, 0, 10);
+        console2.log(amount);
+        amount = bound(amount, 0, weth.balanceOf(address(this)));
         weth.withdraw(amount);
     }
 
