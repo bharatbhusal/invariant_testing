607b2c098b6f84bd36109015ace17f5d20f25e7f
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-03-14 22:44:39 +0530

    fallback

--- test/handles/Handler.sol
+++ test/handles/Handler.sol
@@ -11,6 +11,8 @@ import "forge-std/console2.sol";
 contract Handler is CommonBase, StdCheats, StdUtils {
     WETH9 public weth;
     uint256 public constant ETH_SUPPLY = 120_500_000 ether;
+    uint256 public ghost_depositSum;
+    uint256 public ghost_withdrawSum;
 
     constructor(WETH9 _weth) {
         weth = _weth;
@@ -21,14 +23,24 @@ contract Handler is CommonBase, StdCheats, StdUtils {
         // amount = bound(amount, 0, 10);
         amount = bound(amount, 0, address(this).balance);
         weth.deposit{value: amount}();
+        ghost_depositSum += amount;
     }
 
     function withdraw(uint256 amount) public {
         // bound(amount, 0, 10);
-        console2.log(amount);
+        // console2.log(amount);
         amount = bound(amount, 0, weth.balanceOf(address(this)));
         weth.withdraw(amount);
+        ghost_withdrawSum += amount;
     }
 
     receive() external payable {}
+
+    function sendFallback(uint256 amount) public {
+        console2.log(amount);
+        amount = bound(amount, 0, address(this).balance);
+        (bool success, ) = address(weth).call{value: amount}("");
+        require(success, "sendFallback failed");
+        ghost_depositSum += amount;
+    }
 }
